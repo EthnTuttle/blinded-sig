3e10850f721d82602e4e3ee9c7365a0cfa4f89c8
Ethan Tuttle <tuttle.ethan@protonmail.com>
2024-03-11 08:11:31 -0400

    ai is dumb

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::str::FromStr;
-
 use is_odd::IsOdd;
 use num_bigint::{BigUint, ToBigUint};
-use num_traits::{Euclid, Num};
+use num_traits::Euclid;
 use sha1::{Digest, Sha1};
+
+extern crate tokio;
+use std::sync::Arc;
+use tokio::sync::mpsc;
+use tokio::task;
 
 // const PRIME_ORDER_BIG_UINT: BigUint = BigUint::from_str_radix("512625", 10u32).unwrap();
 const PRIME_ORDER: u64 = 512625;
 // const GENERATOR:  =
-fn main() {
-    let point = hash_to_curve("Hello world!".to_owned());
+
+#[tokio::main]
+async fn main() {
+    let point = hash_to_curve("Hello world!".to_owned()).await;
     println!("{:?} is on the curve", point);
 }
 
@@ -19,49 +24,64 @@ struct Point {
     pub y: u64,
 }
 
-fn hash_to_curve(mut input: String) -> Result<Option<u64>, ()> {
-    let mut value: Option<u64> = None;
-    let mut counter: u64 = 0;
-    loop {
-        // Generate hash
-        let result = Sha1::digest(input);
+async fn hash_to_curve(mut input: String) -> Result<Option<u64>, ()> {
+    let (tx, mut rx) = mpsc::channel(1000);
 
-        let midpoint = result.len() / 2;
-        let first_half = &result[..midpoint];
-        let second_half = &result[midpoint..];
+    let primes = Arc::new((0..PRIME_ORDER as usize).collect::<Vec<usize>>());
+    let primes_ref = Arc::clone(&primes);
 
-        let mut buf: Vec<u8> = Vec::new();
-        buf.extend_from_slice(first_half);
-        let x = BigUint::from_bytes_be(&buf);
+    task::spawn(async move {
+        for prime in primes_ref.iter() {
+            let mut value: Option<u64> = None;
+            let mut counter: u64 = 0;
+            let mut input = format!("{}.{}", prime, "Hello world!");
+            loop {
+                // Generate hash
+                let result = Sha1::digest(input);
 
-        buf.clear();
-        buf.extend_from_slice(second_half);
-        let y = BigUint::from_bytes_be(&buf);
-        let x = Euclid::rem_euclid(&x, &PRIME_ORDER.to_biguint().unwrap());
-        let y = Euclid::rem_euclid(&y, &PRIME_ORDER.to_biguint().unwrap());
+                let midpoint = result.len() / 2;
+                let first_half = &result[..midpoint];
+                let second_half = &result[midpoint..];
 
-        let point = Point {
-            x: u64::try_from(x.clone()).map_err(|_| ())?,
-            y: u64::try_from(y.clone()).map_err(|_| ())?,
-        };
+                let mut buf: Vec<u8> = Vec::new();
+                buf.extend_from_slice(first_half);
+                let x = BigUint::from_bytes_be(&buf);
 
-        if is_on_curve(&point) {
-            let formatted_point = format!(
-                "{}{}",
-                if IsOdd::is_odd(&point.y) { "02" } else { "03" },
-                point.x
-            );
-            value = Some(u64::from_str_radix(&formatted_point, 16).unwrap());
-            break;
+                buf.clear();
+                buf.extend_from_slice(second_half);
+                let y = BigUint::from_bytes_be(&buf);
+                let x = Euclid::rem_euclid(&x, &PRIME_ORDER.to_biguint().unwrap());
+                let y = Euclid::rem_euclid(&y, &PRIME_ORDER.to_biguint().unwrap());
+
+                let point = Point {
+                    x: u64::try_from(x.clone()).unwrap(),
+                    y: u64::try_from(y.clone()).unwrap(),
+                };
+
+                if is_on_curve(&point) {
+                    let formatted_point = format!(
+                        "{}{}",
+                        if IsOdd::is_odd(&point.y) { "02" } else { "03" },
+                        point.x
+                    );
+                    value = Some(u64::from_str_radix(&formatted_point, 16).unwrap());
+                    break;
+                }
+                input = (x + BigUint::new(vec![1])).to_string();
+                counter += 1;
+                if counter == PRIME_ORDER {
+                    break;
+                }
+            }
+            tx.send(value).await.expect("Failed to send message");
         }
-        input = (x + BigUint::new(vec![1])).to_string();
-        counter += 1;
-        if counter == u64::MAX {
-            break;
-        }
+    });
+
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {:?}", message);
+        return Ok(message);
     }
-    println!("{}", counter);
-    Ok(value)
+    Ok(None)
 }
 
 fn is_on_curve(point: &Point) -> bool {
